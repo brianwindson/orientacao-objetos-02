a0a99fd1b4ed6dd21edae6b0f73c617cb0661187
brian <brianwindson@gmail.com>
2020-03-05 03:30:31 -0300

    criação classes

---
+++ src/main/java/exercicios/Quadrado.java
@@ -0,0 +1,13 @@
+package exercicios;
+
+public class Quadrado { public Double lado;
+
+    public Double calcularArea() {
+        return lado * lado;
+    }
+
+    public Double calcularPerimetro() {
+        return 4 * lado;
+    }
+}
+
